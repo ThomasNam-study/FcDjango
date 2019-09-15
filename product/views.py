77c034936e658651870d2ab4c68378bad8dafa6e
남한희 <purred@naver.com>
2019-09-15 17:44:48 +0900

    상품 등록 작업 완료

--- product/views.py
+++ product/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView, FormView
+
+from product.forms import RegisterForm
+from product.models import Product
+
+
+class ProductList(ListView):
+    model = Product
+    template_name = "product.html"
+    context_object_name = "product_list"
+
+
+class ProductCreate(FormView):
+    template_name = "regist_product.html"
+    form_class = RegisterForm
+    success_url = "/product/"
